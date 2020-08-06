383742a979ab0aee2d26c0fd8d875fb6a0550102
zapzrat <zapzrat@gmail.com>
2020-08-06 17:51:37 +0700

    Add files via upload

---
+++ Code/IR_Sensor_example1/IR_Sensor_example1.ino
@@ -0,0 +1,25 @@
+const int sensorIR = 2;
+const int ledStatus =  13;
+int sensorIR_State = 0;
+int count_c = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Micro Project Thailand ~^.^~ ");
+  pinMode(ledStatus, OUTPUT);
+  pinMode(sensorIR, INPUT);
+}
+
+void loop() {
+  sensorIR_State = digitalRead(sensorIR);
+  if (sensorIR_State == HIGH) {
+    digitalWrite(ledStatus, HIGH);
+    count_c = 0;
+  } else {
+    digitalWrite(ledStatus, LOW);
+    count_c ++ ;
+    Serial.print("Sensor Active : ");
+    Serial.println(count_c);
+
+  }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# IR_Sensor
+
+
+![TM1637](IR_InfraredPhotoelectricSensorModule/img/IR_Sensor.jpg)
